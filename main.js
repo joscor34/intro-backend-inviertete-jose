94d40e8dcc7b7c6b9f67b5fda0ebc35448c72df8
juxi <juxi34@localhost.localdomain>
2019-10-23 17:54:07 -0500

    Archivo .gitignore agregado

--- main.js
+++ main.js
@@ -9,6 +9,15 @@ const app = express()
 
 var port = 8080
 
+app.get('/',(req, res) => {
+  res.send('Entraste a la ruta: /')
+})
+
+app.get('/jose', (req, res) => {
+  res.send('Mi nombre es jose')
+})
+
+
 app.listen(port, () => {
   console.log(colors.blue("Tu API esta corriendo en: ") + colors.red('http://localhost:8080'))
 })
